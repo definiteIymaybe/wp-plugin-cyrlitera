ab0ae3f4317453402f00325b94b6aa3b774c4da1
alex.kovalevv <alex.kovalevv@gmail.com>
2020-12-16 06:48:02 +0300

    Отключен поиск по опциям для одиночной версии плагина.

--- admin/pages/class-page-cyrlitera.php
+++ admin/pages/class-page-cyrlitera.php
@@ -69,6 +69,7 @@ class WCTR_CyrliteraPage extends Wbcr_FactoryClearfy000_PageBase {
 			$this->menu_target                = 'options-general.php';
 			$this->add_link_to_plugin_actions = true;
 			$this->page_parent_page           = null;
+			$this->show_search_options_form = false;
 		}
 
 		parent::__construct( $plugin );
